2afded16bc32dfca190d533dbe5b654aa76fcd8a
kaupunki-apina <tomy_salminen@msn.com>
2017-01-07 21:35:59 +0200

    Improved error handling

--- src/server.js
+++ src/server.js
@@ -6,64 +6,54 @@ import EventController from './controllers/EventController';
 import config from './config';
 
 
-(function () {
-  const app = express();
-  const server = http.createServer(app);
-  const router = express.Router();
+const app = express();
+const server = http.createServer(app);
+const router = express.Router();
 
-  app.use(bodyParser.json());
-  app.use(router);
-  app.set('json spaces', 2); // Pretty print
+app.use(bodyParser.json());
+app.use(router);
+app.use((error, req, res, next) => { // eslint-disable-line
+  console.error(error);
+  if (req.app.get('env') !== 'development') {
+    delete error.stack; // eslint-disable-line
+  }
+  res.status(error.statusCode || 500).json(error);
+});
 
-  router.route('/api/v1/event')
-    .post((req, res) => {
-      EventController.createEvent(req.body).then((event) => {
-        res.json(event);
-      }).catch((error) => {
-        console.log(error); // TODO Better error logging
-        res.json({});
-      });
-    });
 
-  router.route('/api/v1/event/list')
-    .get((req, res) => {
-      EventController.getEvents().then((events) => {
-        res.json(events);
-      }).catch((error) => {
-        console.log(error); // TODO Better error logging
-        res.json([]);
-      });
-    });
+router.route('/api/v1/event')
+  .post((req, res, next) => {
+    EventController.createEvent(req.body).then((event) => {
+      res.json(event);
+    }).catch(next);
+  });
 
-  router.route('/api/v1/event/:id')
-    .get((req, res) => {
-      EventController.getEvent(req.params.id).then((event) => {
-        res.json(event);
-      }).catch((error) => {
-        console.log('error: ', error); // TODO Better error logging
-        res.json({});
-      });
-    });
+router.route('/api/v1/event/list')
+  .get((req, res, next) => {
+    EventController.getEvents().then((events) => {
+      res.json(events);
+    }).catch(next);
+  });
 
-  router.route('/api/v1/event/:id/vote')
-    .post((req, res) => {
-      EventController.voteOnEvent(req.params.id, req.body).then((event) => {
-        res.json(event);
-      }).catch((error) => {
-        console.log('error', error); // TODO Better error logging
-        res.json({});
-      });
-    });
+router.route('/api/v1/event/:id')
+  .get((req, res, next) => {
+    EventController.getEvent(req.params.id).then((event) => {
+      res.json(event);
+    }).catch(next);
+  });
 
-  router.route('/api/v1/event/:id/results')
-    .get((req, res) => {
-      EventController.getEventResults(req.params.id).then((results) => {
-        res.json(results);
-      }).catch((error) => {
-        console.log('error', error); // TODO Better error logging
-        res.json({});
-      });
-    });
+router.route('/api/v1/event/:id/vote')
+  .post((req, res, next) => {
+    EventController.voteOnEvent(req.params.id, req.body).then((event) => {
+      res.json(event);
+    }).catch(next);
+  });
 
-  server.listen(config.port, () => console.log('Server listening on: http://localhost:%s', config.port));
-}());
+router.route('/api/v1/event/:id/results')
+  .get((req, res, next) => {
+    EventController.getEventResults(req.params.id).then((results) => {
+      res.json(results);
+    }).catch(next);
+  });
+
+server.listen(config.port);
